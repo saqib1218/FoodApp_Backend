a0577d4e435e14b6fe25236a391e95b0e605d6ca
sana4560 <sanawaqar4560@gmail.com>
2025-08-27 18:07:17 +0500

    all routes fixed

--- src/controllers/admin/users/getUserByIdController.js
+++ src/controllers/admin/users/getUserByIdController.js
@@ -6,7 +6,7 @@ exports.getUserById = async (req, res, next) => {
   const startTime = Date.now();
 
   try {
-    const targetUserId = req.params.userId;
+    const targetUserId = req.params.id;
 
     // Optional: validate UUID format
     const uuidRegex = /^[0-9a-f]{8}-[0-9a-f]{4}-[1-5][0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$/i;
